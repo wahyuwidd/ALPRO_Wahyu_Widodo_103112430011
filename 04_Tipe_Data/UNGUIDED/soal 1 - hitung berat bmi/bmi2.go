7e0cc8cb64164ebe0787468cf317b4e7b638b625
wwxxc <woianjur@gmail.com>
2024-10-10 20:57:59 +0700

    initial commit

--- 04_Tipe_Data/UNGUIDED/soal 1 - hitung berat bmi/bmi2.go
+++ 04_Tipe_Data/UNGUIDED/soal 1 - hitung berat bmi/bmi2.go
@@ -7,6 +7,6 @@ import (
 func main() {
 	var bmi, tinggiBadan, beratBadan float64
 	fmt.Scan(&bmi, &tinggiBadan)
-	beratBadan = bmi*tinggiBadan*tinggiBadan + 1
-	fmt.Println(int(beratBadan))
+	beratBadan = bmi * tinggiBadan * tinggiBadan //rumus hitung bb dari nilai bmi dan tinggi badan
+	fmt.Println(int(beratBadan + 1))             //karena menggunakan int di bulatkan kebawah jadi di tambah dengan 1
 }
